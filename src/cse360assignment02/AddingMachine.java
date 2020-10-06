3438cb38610ef8dc890ee7eab437fd9453cb3790
joseph <josephuang0327@gmail.com>
2020-10-05 21:06:26 -0700

    fixed some error and add more codes.

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -7,30 +7,36 @@ public class AddingMachine
 
 
 	  private int total;
-	  private String operation = "0";
+	  private String operation = "0";	// variable to store all operations
 
 	  public static void main(String[] args) 
 	  {
+		  // initialize variables
 		  int num;
 		  char option;
 		  boolean quit = false;
 		  
+		  // initialize object and scanner
 		  AddingMachine am = new AddingMachine();
 		  Scanner scan = new Scanner(System.in);
 		  
+		  // loop until is quit
 		  while(quit == false)
 		  {
+			  //print menu
 			  System.out.println("What do you like to do?");
 			  System.out.println("a - add");
 			  System.out.println("c - clear memory");
 			  System.out.println("s - subtract");
 			  System.out.println("p - print");
 			  System.out.println("q - quit");
-			  option = scan.next().charAt(0);
-			  option = Character.toLowerCase(option);
-			  switch(option)
+			  
+			  option = scan.next().charAt(0);	//take a char
+			  option = Character.toLowerCase(option);	//make it lower case
+			  
+			  switch(option) // check which character is entered;
 			  {
-			  		case 'a':
+			  		case 'a':	// adding
 			  			  System.out.println("Enter a number: ");
 						  num = scan.nextInt();
 						  am.add(num);
@@ -38,11 +44,11 @@ public class AddingMachine
 						  am.operation = am.toString(num, true, am.operation);
 			  			break;
 			  			
-			  		case 'c':
+			  		case 'c':	//clear
 			  			  System.out.println("Memory clear!");
 			  			  am.clear();
 			  			break;
-			  		case 's':
+			  		case 's':	//subtract
 			  			  System.out.println("Enter a number: ");
 						  num = scan.nextInt();
 						  am.subtract(num);
@@ -50,19 +56,22 @@ public class AddingMachine
 						  am.operation = am.toString(num, false, am.operation);
 			  			break;
 			  			
-			  		case 'p':
+			  		case 'p'://print
 			  			 System.out.println(am.operation + "\n");
 			  			break;
 			  			
-			  		case 'q':
+			  		case 'q':	// quit
 			  				quit = true;
+			  			break;
+			  		default: // defualt
+			  			System.out.println("Unrecognize action!");
 			  			break;
 			  }
 
 		  }
 	  }
 	  
-	  public AddingMachine () 
+	  public AddingMachine ()
 	  {
 	    total = 0;  // not needed - included for clarity
 	  }
@@ -72,19 +81,19 @@ public class AddingMachine
 	    return total;
 	  }
 	  
-	  public void add (int value) 
+	  public void add (int value) //add function
 	  {
 		  total = total + value;
 	  }
 
-	  public void subtract (int value) 
+	  public void subtract (int value) //subtract function
 	  {
 		  total = total - value;
 	  }
 
-	  public String toString (int value, boolean check, String list) 
+	  public String toString (int value, boolean check, String list) //add operation to variable
 	  {
-		if(check == true)
+		if(check == true)	// check if it is adding
 		{
 			list = list + " + " + value;
 			return list;
@@ -93,7 +102,7 @@ public class AddingMachine
 		return list;
 	  }
 
-	  public void clear() 
+	  public void clear() //clear the memory
 	  {
 		  operation = "0";
 		  total = 0;
